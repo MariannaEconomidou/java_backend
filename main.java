7c4d90ea7a01e0ffa7cd49ea75bfec4666262b71
EcnMarianna <45993043+MariannaEconomidou@users.noreply.github.com>
2024-03-03 15:24:44 +0200

    CLIENT SOCKET
    SOCKET,INPUT OUTPUT STREAM HANDLING,CONNECTION WITH TIMEOUT

--- main.java
+++ main.java
@@ -1,44 +1,67 @@
-package inetaddr;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.net.Socket;
+package Sockets;
+
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class main {
-	public static void print(InetAddress address) {
-		System.out.println(address);
-		System.out.println(address.getHostAddress());
-		System.out.print(address.getHostName());
-		System.out.println(Arrays.toString(address.getAddress()));
-		System.out.println("=".repeat(20));
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+     
+		try(   //establish scoket communtication and wrap input and output streams to specific wrappers
+				Socket s=new Socket("www.google.com",80);
+				Scanner in=new Scanner(s.getInputStream());
+				DataOutputStream out=new DataOutputStream(s.getOutputStream());
+				)
+		{
+			out.writeUTF("hey"); //send message that does not apply to http protocol
+			String response=in.nextLine(); // get answer by just reading the input stream
+			System.out.println(response);
+			//other Socket methods
+			System.out.println(s.isClosed());
+			System.out.println(s.isConnected());
+			System.out.println(s.isInputShutdown());
+			System.out.println(s.isOutputShutdown());
+		}
+		catch(UnknownHostException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("===================2====================================");
+	 //Server Connection with Timeout for simple connection
+		try(Socket s2=new Socket())
+		{
+			//connection attempt inside 
+			s2.connect(new InetSocketAddress("www.google.gr",90081),1000);
+		} catch (SocketTimeoutException e) {
+			// TODO Auto-generated catch block
+			System.out.print("Connection time expired");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		System.out.println("========================3===============================");
+		//Establish two way connection with timeout for reads
+		
+		try(Socket s3=new Socket("www.google.com",80);
+			Scanner in=new Scanner(s3.getInputStream());
+			DataOutputStream out=new DataOutputStream(s3.getOutputStream());)
+		{
+		  s3.setSoTimeout(10);
+		  out.writeUTF("Blue");
+		  System.out.println(in.nextLine());
+		}catch(Exception e)
+		{System.out.println(e);}
+		
+		
+		
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       try {
-    	  InetAddress address=InetAddress.getLocalHost();
-    	  print(address);
-    	  address=InetAddress.getByName("www.google.com");
-    	  System.out.println(address);
-          byte[] byteAddress=address.getAddress();
-          print(address);
-          
-          
-    	  
-       }
-       catch(UnknownHostException e) {
-    	   System.out.println(e);	}
-       
-       
-       try(Socket s=new Socket("www.google.com",80)){
-    	   System.out.println(s.getInetAddress());
-       }
-       catch(UnknownHostException e) {
-    	   e.printStackTrace();
-       }
-       catch(IOException e) {
-    	   e.printStackTrace();
-       }
-
-}}
+}
